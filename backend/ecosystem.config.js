006d2a5e2e4ad0cf24f269db7dc57bf0683a4cee
Brian Koupal <bkoupal@gmail.com>
2025-08-20 16:44:45 -0700

    Configure prod envionrment files

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -5,11 +5,11 @@ module.exports = {
       script: "./lib/index.js",
       instances: 1,
       exec_mode: "fork",
-      env_file: "./.env.production",
       env: {
         NODE_ENV: "production",
         PORT: 2567,
         HOST: "0.0.0.0",
+        HOSTNAME: "0.0.0.0",
       },
       error_file: "./logs/err.log",
       out_file: "./logs/out.log",
